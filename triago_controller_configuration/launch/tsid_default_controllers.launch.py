fa14ef9b0c9f36741a75d18fc71fd63cdccf16eb
vivianamorlando <viviana.morlando@pal-robotics.com>
2025-05-14 12:56:35 +0200

    Adding config file for cartesian vel in local frame

--- triago_controller_configuration/launch/tsid_default_controllers.launch.py
+++ triago_controller_configuration/launch/tsid_default_controllers.launch.py
@@ -135,9 +135,14 @@ def setup_arm_controllers(context, arm_side, *args, **kwargs):
         load_gains_separately=True,
     )
 
-    cartesian_vel = setup_arm_side_controller(
-        context, "cartesian_vel_controller", arm_side, load_gains_separately=True
+    cartesian_vel_ee_frame = setup_arm_side_controller(
+        context, "cartesian_vel_controller_ee_frame", arm_side, load_gains_separately=True
     )
+
+    cartesian_vel_robot_frame = setup_arm_side_controller(
+        context, "cartesian_vel_controller_robot_frame", arm_side, load_gains_separately=True
+    )
+
     joint_space_controller_vel = setup_arm_side_controller(
         context, "joint_space_controller_vel", arm_side, load_gains_separately=True
     )
@@ -146,7 +151,8 @@ def setup_arm_controllers(context, arm_side, *args, **kwargs):
     )
 
     return [
-        cartesian_vel,
+        cartesian_vel_ee_frame,
+        cartesian_vel_robot_frame,
         joint_space_controller_vel,
         joint_space_controller,
         cartesian_space_controller_ee_frame,
